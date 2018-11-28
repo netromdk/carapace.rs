bc9d1d537cbf04cc3f7983a6ac3f47e446cbf3a5
Morten Kristensen <msk@nullpointer.dk>
2018-11-28 20:54:24 +0100

    Defined Prompt that yields parsed commands

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,28 @@
 //! Carapace is a general-purpose shell implementation done purely in Rust.
 
 mod command;
+mod prompt;
+
+use prompt::Prompt;
 
 /// Starts the read-eval-print-loop of the Carapace shell.
-pub fn repl() {}
+pub fn repl() {
+    let prompt = Prompt::new();
+
+    loop {
+        let cmd = prompt.parse_command();
+        if let Err(_) = cmd {
+            continue;
+        }
+        let cmd = cmd.unwrap();
+
+        match cmd.name.as_ref() {
+            "exit" | "quit" => break,
+
+            cmd @ _ => {
+                println!("Unknown command: {}", cmd);
+                continue;
+            }
+        }
+    }
+}
---
+++ src/prompt.rs
@@ -0,0 +1,34 @@
+use command::Command;
+
+use std::error::Error;
+use std::io::{self, Write};
+
+/// Controls showing the prompt and yielding lines from stdin.
+pub struct Prompt;
+
+impl Prompt {
+    pub fn new() -> Prompt {
+        Prompt
+    }
+
+    /// Shows prompt and reads command and arguments from stdin.
+    pub fn parse_command(&self) -> Result<Command, Box<dyn Error>> {
+        print!("carapace> ");
+
+        if let Err(error) = io::stdout().flush() {
+            println!("Could not flush: {}", error);
+        }
+
+        let mut input = String::new();
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => {
+                let values: Vec<&str> = input.trim().split_whitespace().collect();
+                Command::new(values)
+            }
+            Err(error) => {
+                println!("Error: {}", error);
+                Err(Box::new(error))
+            }
+        }
+    }
+}
