ec3f72056b40d0c1d65b52ef6b26fa4393b9e5a2
Morten Kristensen <msk@nullpointer.dk>
2018-11-29 23:05:07 +0100

    Propagate exit code of quit/exit commands as shell exit code

--- src/lib.rs
+++ src/lib.rs
@@ -8,22 +8,31 @@ pub mod prompt;
 
 use prompt::Prompt;
 
+use std::process;
+
 /// Starts the read-eval-print-loop of the Carapace shell.
 pub fn repl() {
-    let prompt = Prompt::new();
+    let exit_code;
 
-    loop {
-        let cmd = prompt.parse_command();
-        if let Err(err) = cmd {
-            println!("{}", err);
-            continue;
-        }
+    {
+        let prompt = Prompt::new();
+        loop {
+            let cmd = prompt.parse_command();
+            if let Err(err) = cmd {
+                println!("{}", err);
+                continue;
+            }
 
-        match cmd.unwrap().execute() {
-            Ok(_) => continue,
-            Err(_exit_code) => {
-                break;
+            match cmd.unwrap().execute() {
+                Ok(_) => continue,
+                Err(code) => {
+                    exit_code = code;
+                    break;
+                }
             }
         }
     }
+
+    // Exits process immediately so things must be cleaned up before here!
+    process::exit(exit_code);
 }
