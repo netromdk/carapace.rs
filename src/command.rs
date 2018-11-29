c1024190713034f43e3e1972db11260599f4f6aa
Morten Kristensen <msk@nullpointer.dk>
2018-11-29 21:00:45 +0100

    Moved command execution into Command::execute()

--- src/command.rs
+++ src/command.rs
@@ -1,5 +1,14 @@
+use std::env;
 use std::error::Error;
 use std::fmt;
+use std::path::Path;
+use std::process;
+
+fn set_cwd(dir: &Path) {
+    if let Err(err) = env::set_current_dir(dir) {
+        println!("Could not change to {}: {}", dir.display(), err);
+    }
+}
 
 #[derive(Debug)]
 pub struct Command {
@@ -8,7 +17,7 @@ pub struct Command {
 }
 
 impl Command {
-    /// Parses a vector of strings into a `Command` with command name and arguments.
+    /// Parses a vector of strings into a `Command` with command program and arguments.
     pub fn new(values: Vec<&str>) -> Result<Command, Box<dyn Error>> {
         if values.len() == 0 {
             return Err(Box::new(NoCommandError));
@@ -18,6 +27,47 @@ impl Command {
             program: values[0].to_string(),
             args: values[1..].iter().map(|x| x.to_string()).collect(),
         })
+    }
+
+    /// Execute command and return `Ok(true)` if command was run successfully, `Ok(false)` if not,
+    /// and `Err(exit_code)` on "exit" or "quit".
+    pub fn execute(&self) -> Result<bool, i32> {
+        match self.program.as_ref() {
+            "exit" | "quit" => Err(0),
+
+            "cd" => {
+                let path = if self.args.len() > 0 {
+                    Path::new(&self.args[0])
+                } else {
+                    Path::new("~")
+                };
+
+                let home_dir = dirs::home_dir().unwrap_or_default();
+                if path.starts_with("~") {
+                    set_cwd(&home_dir.join(path.strip_prefix("~").unwrap()));
+                } else {
+                    set_cwd(path);
+                }
+                Ok(true)
+            }
+
+            _ => {
+                // Run command with arguments and wait for it to finish.
+                let output = process::Command::new(&self.program)
+                    .args(&self.args)
+                    .output();
+                match output {
+                    Ok(output) => {
+                        print!("{}", String::from_utf8_lossy(&output.stdout));
+                        Ok(true)
+                    }
+                    Err(err) => {
+                        println!("{}", err);
+                        Ok(false)
+                    }
+                }
+            }
+        }
     }
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -8,10 +8,6 @@ mod prompt;
 
 use prompt::Prompt;
 
-use std::env;
-use std::path::Path;
-use std::process;
-
 /// Starts the read-eval-print-loop of the Carapace shell.
 pub fn repl() {
     let prompt = Prompt::new();
@@ -21,40 +17,13 @@ pub fn repl() {
         if let Err(_) = cmd {
             continue;
         }
-        let cmd = cmd.unwrap();
 
-        match cmd.program.as_ref() {
-            "exit" | "quit" => break,
-
-            "cd" => {
-                let path = if cmd.args.len() > 0 {
-                    Path::new(&cmd.args[0])
-                } else {
-                    Path::new("~")
-                };
-
-                let home_dir = dirs::home_dir().unwrap_or_default();
-                if path.starts_with("~") {
-                    set_cwd(&home_dir.join(path.strip_prefix("~").unwrap()));
-                } else {
-                    set_cwd(path);
-                }
-            }
-
-            _ => {
-                // Run command with arguments and wait for it to finish.
-                let output = process::Command::new(cmd.program).args(cmd.args).output();
-                match output {
-                    Ok(output) => print!("{}", String::from_utf8_lossy(&output.stdout)),
-                    Err(err) => println!("{}", err),
-                }
+        match cmd.unwrap().execute() {
+            Ok(ret) => println!("ret: {}", ret),
+            Err(exit_code) => {
+                println!("exit with {}", exit_code);
+                break;
             }
         }
-    }
-}
-
-fn set_cwd(dir: &Path) {
-    if let Err(err) = env::set_current_dir(dir) {
-        println!("Could not change to {}: {}", dir.display(), err);
     }
 }
