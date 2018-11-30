5d96193f8f6b8b5e1838f440c0e0d24a04edc40f
Morten Kristensen <msk@nullpointer.dk>
2018-11-30 23:17:10 +0100

    Keep input history in ~/.carapace/history

--- src/lib.rs
+++ src/lib.rs
@@ -8,14 +8,22 @@ pub mod prompt;
 
 use prompt::Prompt;
 
+use std::fs;
 use std::process;
 
 /// Starts the read-eval-print-loop of the Carapace shell.
 pub fn repl() {
+    // Create init folder if not present.
+    let path = dirs::home_dir().unwrap().join(".carapace");
+    if let Err(err) = fs::create_dir_all(&path) {
+        println!("Could not create init folder: {}\n{}", path.display(), err);
+        return;
+    }
+
     let exit_code;
 
     {
-        let prompt = Prompt::new();
+        let mut prompt = Prompt::new();
         loop {
             let cmd = prompt.parse_command();
             if let Err(err) = cmd {
--- src/prompt.rs
+++ src/prompt.rs
@@ -3,16 +3,54 @@ use command::{self, Command};
 use std::env;
 use std::error::Error;
 use std::fmt;
+use std::fs;
 use std::io::{self, Write};
 
 use term;
 
 /// Controls showing the prompt and yielding lines from stdin.
-pub struct Prompt;
+pub struct Prompt {
+    /// History of inputs.
+    history: Vec<String>,
+
+    /// Maximum of newest entries to keep in history.
+    history_max: usize,
+}
 
 impl Prompt {
     pub fn new() -> Prompt {
-        Prompt
+        let mut p = Prompt {
+            history: vec![],
+            history_max: 1000,
+        };
+        p.load_history();
+        p
+    }
+
+    /// Shows prompt and reads command and arguments from stdin.
+    pub fn parse_command(&mut self) -> Result<Box<dyn Command>, Box<dyn Error>> {
+        self.show()?;
+
+        let mut input = String::new();
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => {
+                let input = input.trim();
+                let values: Vec<&str> = input.split_whitespace().collect();
+                if values.len() == 0 {
+                    return Err(Box::new(NoCommandError));
+                }
+
+                self.push_to_history(input.to_string());
+
+                let program = values[0].to_string();
+                let args = values[1..].iter().map(|x| x.to_string()).collect();
+                command::parse_command(program, args)
+            }
+            Err(error) => {
+                println!("Error: {}", error);
+                Err(Box::new(error))
+            }
+        }
     }
 
     /// Shows prompt.
@@ -35,27 +73,37 @@ impl Prompt {
         Ok(())
     }
 
-    /// Shows prompt and reads command and arguments from stdin.
-    pub fn parse_command(&self) -> Result<Box<dyn Command>, Box<dyn Error>> {
-        self.show()?;
-
-        let mut input = String::new();
-        match io::stdin().read_line(&mut input) {
-            Ok(_) => {
-                let values: Vec<&str> = input.trim().split_whitespace().collect();
-                if values.len() == 0 {
-                    return Err(Box::new(NoCommandError));
-                }
-
-                let program = values[0].to_string();
-                let args = values[1..].iter().map(|x| x.to_string()).collect();
-                command::parse_command(program, args)
-            }
-            Err(error) => {
-                println!("Error: {}", error);
-                Err(Box::new(error))
-            }
+    fn push_to_history(&mut self, input: String) {
+        self.history.push(input);
+        if self.history.len() > self.history_max {
+            self.history.remove(0);
         }
+    }
+
+    fn load_history(&mut self) {
+        let path = dirs::home_dir().unwrap().join(".carapace").join("history");
+        match fs::read(&path) {
+            Ok(contents) => {
+                self.history = String::from_utf8_lossy(&contents)
+                    .split('\n')
+                    .map(|x| x.to_string())
+                    .collect();
+            }
+            Err(err) => println!("Could not load history from: {}\n{}", path.display(), err),
+        }
+    }
+
+    fn save_history(&self) {
+        let path = dirs::home_dir().unwrap().join(".carapace").join("history");
+        if let Err(err) = fs::write(&path, self.history.join("\n")) {
+            println!("Could not write history to: {}\n{}", path.display(), err);
+        }
+    }
+}
+
+impl Drop for Prompt {
+    fn drop(&mut self) {
+        self.save_history();
     }
 }
 
