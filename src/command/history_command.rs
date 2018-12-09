f6ce6cf8232854005e83924365f6533fb112921c
Morten Kristensen <msk@nullpointer.dk>
2018-12-09 01:02:03 +0100

    Don't create history App for each execute() invocation

--- src/command/history_command.rs
+++ src/command/history_command.rs
@@ -5,32 +5,35 @@ use clap::{App, AppSettings, Arg};
 /// History command shows the list of inputs.
 pub struct HistoryCommand {
     vars: Vec<String>,
+    app: App<'static, 'static>,
 }
 
 impl HistoryCommand {
     pub fn new(args: Vec<String>) -> HistoryCommand {
-        HistoryCommand { vars: args }
+        HistoryCommand {
+            vars: args,
+            app: App::new("history")
+                .about("When no options are specified, all history items will be listed.")
+                .setting(AppSettings::NoBinaryName)
+                .setting(AppSettings::DisableVersion)
+                .arg(
+                    Arg::with_name("clear")
+                        .short("c")
+                        .long("clear")
+                        .help("Clear current session's history (not what's saved on disk)."),
+                ).arg(
+                    Arg::with_name("write")
+                        .short("w")
+                        .long("write")
+                        .help("Writes history to disk."),
+                ),
+        }
     }
 }
 
 impl Command for HistoryCommand {
-        let matches = App::new("history")
-            .about("When no options are specified, all history items will be listed.")
-            .setting(AppSettings::NoBinaryName)
-            .setting(AppSettings::DisableVersion)
-            .arg(
-                Arg::with_name("clear")
-                    .short("c")
-                    .long("clear")
-                    .help("Clear current session's history (not what's saved on disk)."),
-            ).arg(
-                Arg::with_name("write")
-                    .short("w")
-                    .long("write")
-                    .help("Writes history to disk."),
-            ).get_matches_from_safe(&self.vars);
-
     fn execute(&mut self, prompt: &mut Prompt) -> Result<bool, i32> {
+        let matches = self.app.get_matches_from_safe_borrow(&self.vars);
         if matches.is_err() {
             println!("{}", matches.unwrap_err());
             return Ok(false);
